b5a517f5a4aa9c3ad0c0025a6c411b8672667b06
Giorgio <infinit7even@gmail.com>
2020-06-08 14:13:19 +0000

    Updated the Menu

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="It's-a me, Mario! m!help"))
 
 
-#help
 @bot.command(description='It s-a me, Mario!')
 async def help(ctx):
     embed = discord.Embed(
@@ -38,7 +37,13 @@ async def help(ctx):
                     name=f"{bot.command_prefix}{x.name}",
                     value=f'```{x.description}```',
                     inline=False)
-    await ctx.author.send(embed=embed, delete_after=90)
+    mes = await ctx.send(embed=embed)
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) == '🔧'  
+    await mes.add_reaction(emoji='🔧')
+    reaction, user = await bot.wait_for('reaction_add', check=check)
+    if reaction.emoji == "🔧":
+        await mes.delete()
 
 #log
 @bot.event
@@ -79,69 +84,68 @@ async def say(ctx, *, message):
 
 @bot.command(description='View support server')
 async def support(ctx):
-
     embed = discord.Embed(
         title="I'm-a-tired.",
         description=
         "Coming soon...",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
 
 @bot.command(description='View source code')
 async def source(ctx):
-
     embed = discord.Embed(
         title="I'm-a-tired.",
         description=
         "The source code is available on [GitHub](https://github.com/Infinit7Even/Mario-)",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
 
 @bot.command(description='Invite Mario to your server')
 async def invite(ctx):
-
     embed = discord.Embed(
         title="Mamma mia!",
         description=
-        "[Invite me!](https://top.gg/bot/714550524829106296)",
+        "[Invite Mario](https://top.gg/bot/714550524829106296) in your server!",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
 
 @bot.command(description='Vote Mario In the Store')
 async def vote(ctx):
-
     embed = discord.Embed(
         title="Thank you so much for-to-playing my game!",
         description="[Vote Mario!](https://top.gg/bot/714550524829106296)",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
 
 @bot.command(description='Bot credits')
 async def credit(ctx):
-
     embed = discord.Embed(
         title="Thank you so much for-to-playing my game!",
         description="Bot developed da **Infinit7Even#1803** and **IT | Kewai#9029**",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
 
 @bot.command(description='View Mario s database')
 async def database(ctx):
-
     embed = discord.Embed(
         title="Here we go!",
         description="Mario s word database is available on [GitHub](https://github.com/Infinit7Even/Mario-/blob/master/main.py)",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
 
 @bot.command(description='Use this command if Mario isn t working properly')
 async def fix(ctx):
-
     embed = discord.Embed(
         title="Nighty, nighty. Ah, spaghetti... ah, ravioli... ah, mamma mia.",
         description="Make sure Mario can read the messages, delete them and send links, if you still have problems contact Infinit7Even#1803.",
         colour=0xFF001E)
-    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=20)
+
+@bot.command(description='Bot response time in ms (Milliseconds)')
+async def ping(ctx):
+    latency = bot.latency
+    await ctx.send('**Bot response time in ms (Milliseconds):**')
+    await ctx.send(latency)
 
 #support
 @bot.event
@@ -152,11 +156,6 @@ async def on_message(message):
         if message.content.lower() == "m!say":
             triggered = ['`To use that command type m!say message`']
             await message.author.send(
-                f"{random.choice(triggered)}")
-
-        if message.content.lower() == "m!help":
-            triggered = ['I wrote to you privately!\nMake sure you have not blocked direct messages in your account discord settings.', 'The menu was sent to you privately.\nMake sure you have not blocked direct messages in your account discord settings.']
-            await message.channel.send(
                 f"{random.choice(triggered)}")
 
 #triggered
